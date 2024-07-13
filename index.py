5224e2b24b8798b7e797c482c1bd7543b4e9f43b
matsuzaki <takuya.matsuzaki@nttsportict.co.jp>
2024-07-13 18:54:45 +0900

    Implement glasses overlay feature

--- index.py
+++ index.py
@@ -4,15 +4,14 @@ import cvzone
 import cv2
 from cvzone.PoseModule import PoseDetector
 
-#cap = cv2.VideoCapture("Resources/Videos/1.mp4")
 cap = cv2.VideoCapture(0)
 detector = PoseDetector()
 
-shirtFolderPath = "Resources/Shirts"
+shirtFolderPath = "Resources/Glasses"
 listShirts = os.listdir(shirtFolderPath)
 # print(listShirts)
-fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
-shirtRatioHeightWidth = 581 / 440
+fixedRatio = 320 / 190  # widthOfShirt/widthOfPoint11to12
+shirtRatioHeightWidth = 150 / 500
 imageNumber = 0
 imgButtonRight = cv2.imread("Resources/button.png", cv2.IMREAD_UNCHANGED)
 imgButtonLeft = cv2.flip(imgButtonRight, 1)
@@ -24,16 +23,13 @@ while True:
     success, img = cap.read()
     if not success:
         break
-    img = detector.findPose(img)
+    img = detector.findPose(img, draw=False)
     # img = cv2.flip(img,1)
     lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
     if lmList:
         # center = bboxInfo["center"]
-        #leftEyeOuter = lmList[3]
-        #rightEyeOuter = lmList[6]
-        #print(leftEyeOuter, rightEyeOuter)
-        lm11 = lmList[11]  # 右肩
-        lm12 = lmList[12]  # 左肩
+        lm11 = lmList[3]  # 右目外側
+        lm12 = lmList[6]  # 左目外側
         print(lm11, lm12)
         imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
 
@@ -47,30 +43,6 @@ while True:
             img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
         except:
             pass
-
-        img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
-        img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
-
-        if lmList[16][1] < 300:
-            counterRight += 1
-            cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
-                        counterRight * selectionSpeed, (0, 255, 0), 20)
-            if counterRight * selectionSpeed > 360:
-                counterRight = 0
-                if imageNumber < len(listShirts) - 1:
-                    imageNumber += 1
-        elif lmList[15][1] > 900:
-            counterLeft += 1
-            cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
-                        counterLeft * selectionSpeed, (0, 255, 0), 20)
-            if counterLeft * selectionSpeed > 360:
-                counterLeft = 0
-                if imageNumber > 0:
-                    imageNumber -= 1
-
-        else:
-            counterRight = 0
-            counterLeft = 0
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
